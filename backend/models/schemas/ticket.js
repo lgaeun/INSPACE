3c7ebb420c78ecb272596e8acbe0ff831db6893e
Your Name <you@your-domain.com>
2021-12-21 15:12:40 +0900

    :construction: Schema 업데이트

--- backend/models/schemas/ticket.js
+++ backend/models/schemas/ticket.js
@@ -1,23 +1,26 @@
 const { Schema } = require("mongoose");
 
-const TicketSchema = new Schema({
+const TicketSchema = new Schema(
+  {
     //이용권 종류
     category: {
-        type: String,
-        required: true,
+      type: String, //oneday , savingUpTime,
+      required: true,
     }, //구매한 이용권의 시간
     duration: {
-        type: Number,
-        required: true,
+      type: Number, // duration: hour
+      required: true,
     }, //이용권가격
     price: {
-        type: Number,
-        rquired: true,
+      type: Number,
+      rquired: true,
     }, //구매한 시간
-    // user: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User",
-    // },
-}, { timestamps: true });
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = TicketSchema;
