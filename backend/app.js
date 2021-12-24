fb4d2d43952ef5d9ca452221fc31f0e063b87893
Your Name <you@your-domain.com>
2021-12-24 16:06:55 +0900

    a

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    resave: false,
+    resave: true,
     saveUninitialized: false,
     cookie: {
       secure: false,
